ef408b8533a7d9f5d0a0edd91f4a2c7ab5ed18c9
jmwang0117 <1021347250@qq.com>
2023-12-02 14:08:42 +0000

    new

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Vi2ENav: A Vision-aided, ESDF-free and Energy-saving Navigation System for Multi
 ## News
 - [2023/11]: The [3D model ](https://connecthkuhk-my.sharepoint.com/:u:/g/personal/u3009632_connect_hku_hk/ERX7ejbV3xdOkLQe5SMgGG0Bh6D1qGd-9vg5iMWpi8VQsw?e=H07haj) in the simulation environment can be downloaded in OneDrive.
 
+catkin_make -j1
+
+catkin_make -DCMAKE_BUILD_TYPE=Release -j1
--- run_sim.sh
+++ run_sim.sh
@@ -7,7 +7,7 @@ tmux new-session -d -s "$SESSION_NAME"
 
 # Execute the commands in each window
 tmux send-keys -t "$SESSION_NAME:0" 'source ~/Vi2ENav/devel/setup.bash; roslaunch ego_planner simple_run.launch' C-m
-#tmux new-window -t "$SESSION_NAME" 'sleep 2; source ~/Vi2ENav/devel/setup.bash; roslaunch ego_planner run_in_sim.launch'
+tmux new-window -t "$SESSION_NAME" 'sleep 2; source ~/Vi2ENav/devel/setup.bash; roslaunch ego_planner rviz.launch'
 tmux new-window -t "$SESSION_NAME" 'sleep 2;rostopic echo /planning/pos_cmd'
 
 # Attach to the tmux session
